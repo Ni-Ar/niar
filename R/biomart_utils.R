719df44b056b3a9b8fcc1f2d6594c522775ccbc2
Ni-Ar <arecco.niccolo@gmail.com>
2022-12-01 21:07:01 +0100

    Improved gimme_mart()
    Now gimme_mart() can query a specific database version instead of the latest or penultimate one.
    Also it returns the genome assembly of that version.
    
    For example to get the last mm10 biomart version use
    `mouse_ensembl <- gimme_mart(species = 'mmusculus', version = 102, verbose = T)`

--- R/biomart_utils.R
+++ R/biomart_utils.R
@@ -1,5 +1,7 @@
 #' Handy function to create a biomaRt R object based on the ENSEMBL database to be used when querying biomart
 #'
+#' @description A function to get a biomaRt object during an R analysis that can later on be used for retrieving gene IDs, coordinates and more.
+#' 
 #' @param species a character specifying the species to work with. Currently the supported species are:
 #' \itemize{
 #' \item{\code{hsapiens} - Human (Homo Sapiens) Default}
@@ -7,8 +9,13 @@
 #' \item{\code{rnorvegicus} - Rat (Rattus Norvegicus)}
 #' \item{\code{ggallus} - Chicken (Gallus Gallus)}
 #' } 
-#' @param latest Logical. Do you want to use the latest biomart version? Default `TRUE`. If `FALSE` penultimate version is used.
-#' @param use_mirror Logical. Do you want to use a mirror for creating the biomart object? Default `FALSE`.
+#' @param version Which database version of biomart do you want? Use:
+#' \itemize{
+#' \item{\code{latest} - for the latest version of the database. }
+#' \item{\code{penultimate} - for the one before the latest version of the database. }
+#' \item{\code{number} - a specific version of the biomart database. If the version you query is not available an error is returned.}
+#' }  
+#' @param use_mirror Logical. Do you want to use a mirror for creating the biomart object? Default `FALSE`. If `TRUE` the parameter version will be converted to `TRUE`.
 #' @param which_mirror Specify which biomart mirror to use. Chose from:
 #' \itemize{
 #' \item{\code{www} - UK (Sanger Institute). in Cambridge, default}
@@ -17,18 +24,18 @@
 #' \item{\code{asia} - Asia (Amazon AWS) in Singapore}
 #' } 
 #' @param out_dir Either `FALSE` (default) or a character specifying a path where to save the biomaRt object. If directory does not exist it will be created.
-#' @param verbose Logical. Print info messages? Default `FALSE`.
+#' @param verbose Logical. Print the version and genome assembly for the species queried. Default `TRUE`.
 #'
 #' @return A biomaRt ensembl object
 #' @importFrom biomaRt useMart listDatasets listEnsemblArchives useEnsembl
 #' @export
 #'
 #' @examples
-#' # Human ENSEMBL biomaRT object
-#' ensembl <- gimme_mart()
-gimme_mart <- function( species = "hsapiens", latest = TRUE, 
-                        use_mirror = FALSE, which_mirror, out_dir = FALSE, 
-                        verbose = FALSE) {
+#' # Human ENSEMBL biomaRt object
+#' human_ensembl <- gimme_mart()
+gimme_mart <- function(species = "hsapiens", version = 'latest', verbose = TRUE,
+                       out_dir = FALSE, use_mirror = FALSE, which_mirror) {
+  # ---- Check input parameters ----
   supported_speciess <- c("hsapiens", "mmusculus", "rnorvegicus", "ggallus")
   if (! species %in% supported_speciess ) {
     stop("The species you selected ", species, " is not yet supported. ",
@@ -37,113 +44,132 @@ gimme_mart <- function( species = "hsapiens", latest = TRUE,
          "Mus Musculus.")
   }
   
-  if (is.null(out_dir)) {
+  if ( is.null(out_dir) ) {
     stop("You didn't specify the out_dir parameter! It must be FALSE to ",
          "keep the biomart object in the environment, or a character (path) ",
          " to specify where to save the biomart object as a R object.")
-    
   }
   
   if (out_dir == TRUE) {
     stop("out_dir parameter is either FALSE or a character specifying the path.")
   }
   
-  if (use_mirror) {
-    if (is.null(which_mirror)) {
+  # -- If using a mirror server the version can only be the latest
+  if ( use_mirror == TRUE ) {
+    if ( is.null(which_mirror) ) {
       stop("You want to use a mirror, but need to specify one from: ",
            "'www', 'uswest', 'useast', 'asia'.")
+    } else if ( which_mirror %in% c('www', 'uswest', 'useast', 'asia') ) {
+      if (version != 'latest') {
+        warning("When specifying a mirror the database version is set to 'latest'!")
+        version <- 'latest'
+      }
+    } else {
+      stop("use_mirror must be either TRUE or FALSE" )
     }
   }
   
-  # ---- Make Biomart object
-  ensembl <- useMart(biomart = "ensembl")
-  datasets <- listDatasets(ensembl)
+  # ---- Get available archived versions of Ensembl ----
   DB_versions <- listEnsemblArchives()
   
   # If retrieve Ensembl Archive fails because of Mart connection
   if ( !exists("DB_versions") ) {
-    # Create a fake database version for human
-    if (species == "hsapiens") {
-      DB_versions <- data.frame(name = c("Ensembl 104", "Ensembl 103"), 
-                                date = c("May 2021", "Feb 2021"),
-                                url = c("https://may2021.archive.ensembl.org", 
-                                        "https://feb2021.archive.ensembl.org"),
-                                version = c(104, 103),
-                                current_release = c("*", "")
-      )
-    }
+    stop("Can't retrieve ENSEMBL Database Archives info! ",
+         "This is usually caused by a connection error or biomart downtime")
   }
   
+  # ---- Get db version ----
+  if( is.numeric(version) == TRUE | is.integer(version) == TRUE ) {
+    
+    DB_v_available <- as.integer(DB_versions$version[-1])
+    
+    if( version < min(DB_v_available) ) {
+      stop("The version you required (v", version, ") is no longer supported! ",
+           "Increase to at least version", min(DB_v_available))
+    }
+    
+    if( version > max(DB_v_available) ) {
+      stop("The version you required (v", version, ") is not yet released! ",
+           "Decrease to at least version ", max(DB_v_available) )
+    }
+    
+    if ( version %in% DB_v_available ) {
+      DB_v <- as.integer(version)
+      # -- Get db version release date
+      DB_v_date <- DB_versions[which(DB_versions$version == DB_v), "date"]
+      
+    } else{
+      stop("The specified version is not currently available! ",
+           "Pick one of these available versions: ",
+           paste0(DB_v_available, sep = ", "))
+    }
+    
+  } else if ( version == 'latest' ) {
+    # -- latest version
+    DB_v <- DB_versions[grep("\\*" , DB_versions$current_release), "version"]
+    DB_v <- as.integer(DB_v)
+    # -- Get db version release date
+    DB_v_date <- DB_versions[grep("\\*" , DB_versions$current_release), "date"]
+    
+  } else if ( version == 'penultimate' ) {
+    # -- penultimate version
+    DB_v <- DB_versions[grep("\\*" , DB_versions$current_release) + 1, "version"]
+    DB_v <- as.integer(DB_v)
+    # -- Get db version release date
+    DB_v_date <- DB_versions[grep("\\*" , DB_versions$current_release) + 1, "date"]
+  } else { 
+    stop("The 'version' argument must be either 'latest' or 'penultimate' or an integer", 
+         "number between ", max(DB_v_available), " and ", min(DB_v_available),
+         " And 'use_mirror' can be used only with version = 'latest' ")
+  }
+  
+  if ( !exists("DB_v") ) {
+    stop("I couldn't get the database version you wanted! ",
+         "The 'version' argument must be either 'latest' or FALSE or an integer", 
+         "number between ", max(DB_v_available), " and ", min(DB_v_available),
+         " And 'use_mirror' can be used only with version = 'latest' ")
+    
+  }
+  
+  DB_v_host_url <- DB_versions[which(DB_versions$version == DB_v), "url"]
+  
+  ensembl <- useMart(biomart = "ensembl", host = DB_v_host_url)
+  datasets <- listDatasets(ensembl)
   # ---- Get species dataset name
   species_ensembl <- datasets[grep(species, datasets$dataset), "dataset" ]
   
   if ( length(species_ensembl) == 0 ) {
     stop("The species you selected ", species, " is wrong or ", 
-         "not supported. Use first letter and second full name of the ", 
-         "species scientific name like 'hsapiens' for Homo Sapiens or ", 
-         " 'mmusculus' for Mus Musculus.")
+         "not supported!")
+  }
+  # ---- Get genome assembly name version
+  genome_assembly_version <- datasets[grep(species, datasets$dataset), "version" ]
+  
+  # ---- Print info ----
+  if (verbose == TRUE) {
+    message("Creating Mart object:\n",
+            "version: ", DB_v, "\n", 
+            "released in: ", DB_v_date, "\n",
+            "dataset: ", species_ensembl, "\n",
+            "genome assembly: ", genome_assembly_version )
   }
   
-  if (!use_mirror) {
-    # -- If using a mirror you cannot specify the version
+  # ---- Create ENSEMBL BioMart Object ----
+  if (use_mirror == TRUE) {
+    # -- Ensembl Dataset from a mirrored server
+    ensembl <- useEnsembl(biomart = "ensembl", 
+                          dataset = species_ensembl,
+                          mirror  = which_mirror)
     
-    # -- Get db version
-    if ( latest == TRUE ) {
-      
-      DB_v <- DB_versions[grep("\\*" , DB_versions$current_release), "version"]
-      DB_v <- as.integer(DB_v)
-      
-      # -- Get db version release date
-      DB_v_date <- DB_versions[grep("\\*" , DB_versions$current_release), "date"]
-      
-      if (verbose) {
-        message("Creating Mart object with latest version ", DB_v, 
-                " released on ", DB_v_date )
-      }
-      
-      DB_v_date <- sub(" ", "_", DB_v_date)
-      
-    } else if (latest == FALSE ) {
-      
-      DB_v <- DB_versions[grep("\\*" , DB_versions$current_release) + 1, "version"]
-      DB_v <- as.integer(DB_v)
-      
-      # -- Get db version release date
-      DB_v_date <- DB_versions[grep("\\*" , DB_versions$current_release) + 1, "date"]
-      if (verbose) {
-        message("Creating Mart object with penultimate version ", DB_v, 
-                " released on ", DB_v_date )
-      }
-      DB_v_date <- sub(" ", "_", DB_v_date)
-      
-    } else { stop("latest argument must be logical either T or F")
-    }
-    
-  }
-  
-  if (use_mirror) {
-    
-    if (which_mirror %in% c('www', 'uswest', 'useast', 'asia') ) {
-      # ---- Ensembl Dataset on a mirro
-      ensembl <- useEnsembl(biomart = "ensembl", 
-                            dataset = species_ensembl,
-                            mirror  = which_mirror)
-      
-    } else {
-      stop("The which_mirror argument must be one of: ",
-           "'www', 'uswest', 'useast', 'asia'.",
-           which_mirror, " is not supported")
-    }
-    
-  } else if (!use_mirror) {
-    # ---- Ensembl Dataset on main host
+  } else if ( use_mirror == FALSE) {
+    # -- Ensembl Dataset on main host
     ensembl <- useEnsembl(biomart = "ensembl", 
                           dataset = species_ensembl, 
                           version = DB_v, 
-                          host = "https://www.ensembl.org")
+                          host = DB_v_host_url)
   }
   
-  # -- Write output or keep in the environment
+  # ---- Write output or keep in the environment ---
   if ( is.character(out_dir) ) {
     
     # ---- Output Directory
@@ -151,22 +177,21 @@ gimme_mart <- function( species = "hsapiens", latest = TRUE,
     # ---- Check if output dir exists, if not create it
     if (!dir.exists(out_biomart_dir)) { dir.create(out_biomart_dir) }
     
-    if (!use_mirror) {
+    if (use_mirror == FALSE) {
+      DB_v_date <- sub(" ", "_", DB_v_date)
       output_ensembl_name <- paste0(out_biomart_dir, "/", 
                                     format(Sys.Date(), "%Y_%m_%d"), "_",
                                     species_ensembl, "_v", DB_v, "_",
                                     DB_v_date, ".rds")
       
-    } else if (use_mirror) {
+    } else if ( use_mirror == TRUE) {
       output_ensembl_name <- paste0(out_biomart_dir, "/", 
                                     format(Sys.Date(), "%Y_%m_%d"), "_",
                                     species_ensembl, "_mirror_", which_mirror,
                                     ".rds")
     } else {
-      stop("Something is wrong when creating a name for the ENSEMBL Object.")
+      stop("Error occurred when preparing to write to file the biomart object.")
     }
-    
-    
     saveRDS(object = ensembl, file = output_ensembl_name)
     if ( verbose ) { message("ENSEMBL Mart Object saved in ", out_biomart_dir) }
     
@@ -225,7 +250,7 @@ ensembl_id_2_gene_name <- function(ensembl_gene_id, only_gene_name = TRUE,
                     filters = 'ensembl_gene_id',
                     values = ensembl_gene_id,
                     mart = mRt_objct,
-                    verbose = verbose,
+                    verbose = FALSE,
                     uniqueRows = T,
                     quote = "\'", 
                     useCache = TRUE)
@@ -288,7 +313,7 @@ gene_name_2_ensembl_id <- function(gene_name, only_ensembl_id = TRUE,
                     filters = 'external_gene_name',
                     values = gene_name,
                     mart = mRt_objct,
-                    verbose = verbose,
+                    verbose = FALSE,
                     uniqueRows = T,
                     quote = "\'", 
                     useCache = TRUE)
@@ -378,7 +403,7 @@ ensembl_id_2_entrez <- function(ensembl_gene_id, only_entrez = TRUE,
                     filters = 'ensembl_gene_id',
                     values = ensembl_gene_id,
                     mart = mRt_objct,
-                    verbose = verbose,
+                    verbose = FALSE,
                     uniqueRows = T,
                     quote = "\'", 
                     useCache = TRUE)
